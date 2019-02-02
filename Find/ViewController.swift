10ebb9022d8b55f818351f7f8ee96f9e41d188f5
Kun Huang <kkun.huang@gmail.com>
2019-02-01 22:25:18 -0500

    Initial Commit

---
+++ Find/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Find
+//
+//  Created by Kun Huang on 2/1/19.
+//  Copyright © 2019 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
