bbea2294502f1875adfbe53cd42d85c7527ddf8d
Kun Huang <kkun.huang@gmail.com>
2019-02-05 22:54:55 -0500

    connect the outlets to controller

--- Find/ViewController.swift
+++ Find/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var jobTitleText: UITextField!
+    @IBOutlet weak var locationText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func searchJobs(_ sender: UIButton) {
+        print("hello")
+    }
+    
 }
 
